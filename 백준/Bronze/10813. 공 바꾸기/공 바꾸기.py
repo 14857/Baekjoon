796d2c8083e686a43aab622bc6e04e2dd3460dcb
14857 <165680202+14857@users.noreply.github.com>
2024-04-03 18:56:29 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,13 @@
+# N,M 입력받기
+n,m = map(int,input().split())
+
+# N개의 바구니 현황
+basket = [i for i in range(1,n+1)]
+
+# M번 동안 공을 넣는 방법 입력받기
+for _ in range(m):
+    i,j = map(int,input().split())
+    basket[i-1],basket[j-1] = basket[j-1],basket[i-1]
+
+# 바구니에 들어있는 공의 번호 출력
+print(*basket)
