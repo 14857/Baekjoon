5ab32ae2b0235e9856f7459ffab998f760cd00f0
14857 <165680202+14857@users.noreply.github.com>
2024-04-17 00:12:09 +0900

    [Bronze III] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,8 @@
+credit = [4.3,4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.3,1.0,0.7,0.0]
+grade = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D+','D0','D-','F']
+
+c = input()
+
+for i in range(len(grade)):
+    if c == grade[i]:
+        print(credit[i])
