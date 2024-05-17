3e91ea2dbb68148f919f6d39aa51c4df6f1cfbe2
14857 <165680202+14857@users.noreply.github.com>
2024-05-17 23:43:27 +0900

    [Silver IV] Title: 문자열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.py
@@ -0,0 +1,15 @@
+ans = []
+
+a,b = input().split()
+
+for i in range (len(b)-len(a)+1):
+    
+    count = 0
+    
+    for j in range(len(a)):
+        if a[j]!= b[i+j]:
+            count += 1
+
+    ans.append(count)
+    
+print(min(ans))
