7df4d2b39c6932ebbf8a0d8420d5c608e82c40da
14857 <165680202+14857@users.noreply.github.com>
2024-06-05 21:09:49 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,33 @@
+# 원에서 사람들이 제거되는 순서를 (N, K)-요세푸스 순열
+# queue 이용
+
+def josephus(queue, k):
+    result = []
+    num = k-1 # 리스트 인덱스
+    
+    for i in range(len(queue)):
+        
+        if (len(queue) > num): # 남은 사람들이 num보다 큰 경우
+            result.append(queue.pop(num)) # 해당되는 수는 result로
+            num += k-1
+            
+        else: # 남은 사람들이 num보다 작은 경우
+            num = num % len(queue)
+            result.append(queue.pop(num))
+            num += k-1
+            
+    return result
+
+# main
+n,k = map(int,input().split())
+queue = [i for i in range(1,n+1)]
+
+result = josephus(queue,k)
+
+# 출력
+ans = str(result[0])
+
+for i in result[1:]:
+    ans += ", " + str(i)
+
+print('<'+ ans +'>')
