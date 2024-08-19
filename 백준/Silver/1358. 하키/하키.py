d1f2fd101c983fbcd2313992e2716eabfe7a7131
14857 <165680202+14857@users.noreply.github.com>
2024-08-19 15:58:33 +0900

    [Silver IV] Title: 하키, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1358. 하키/하키.py
+++ 백준/Silver/1358. 하키/하키.py
@@ -1,21 +1,21 @@
-import sys
-input = sys.stdin.readline
+# P는 선수의 수, 최대 50인 자연수
+# W와 H는 100보다 작거나 같은 자연수, H는 짝수
+# X와 Y는 절댓값이 100보다 작거나 같은 정수
 
-W, H, X, Y, P = map(int, input().split())
+# 링크 안에 있는 선수의 수를 출력
+
+W, H, X, Y, P = map(int,input().split())
 count = 0
-for _ in range(P):
-    x, y = map(int, input().split())
-    
-    # 가운데 직사각형 내부에 있는 경우(둘레 포함)
-    if (X <= x <= X+W) and (Y <= y <= Y+H):
-        count +=1
-        continue
-    
-    # 왼쪽 반원, 오른쪽 반원 내부에 있는 경우(둘레 포함)
-    R = H/2
-    d1 = ((x-X)**2 + (y-(Y+R))**2)**0.5
-    d2 = ((x-(X+W))**2 + (y-(Y+R))**2)**0.5
-    if d1 <= R or d2 <= R:
-        count += 1
 
+for _ in range(P):
+    newx, newy = map(int,input().split())
+    
+    if X <= newx <= X+W  and Y <= newy <= Y+H:
+        count += 1
+    elif (newx-X)**2 + (newy-Y-H/2)**2 <= (H/2)**2:    
+        count += 1
+    
+    elif (newx-X-W)**2 + (newy-Y-H/2)**2 <= (H/2)**2:    
+        count += 1
+    
 print(count)
