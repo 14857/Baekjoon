b458928b7b1ceae97210c659116fd5e137fa3fae
14857 <165680202+14857@users.noreply.github.com>
2024-09-03 00:02:18 +0900

    [Silver III] Title: 어린 왕자, Time: 144 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1004. 어린 왕자/어린 왕자.py
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -1,23 +1,24 @@
-# 테스트 케이스 개수
-T = int(input())
+# 은하수 지도, 출발점, 도착점이 주어졌을 때 최소의 행성계 진입/이탈 횟수를 구하는 프로그램
+# 테스트 케이스의 개수 T / 출발점 (x1, y1) 도착점 (x2, y2) /  행성계의 개수 n / 행성계의 중점과 반지름 (cx, cy, r)
 
-for _ in range(T):
-    # 출발점 도착점
+t = int(input())
+
+for _ in range(t):
     x1, y1, x2, y2 = list(map(int, input().split()))
-    # 행성계의 개수
     n = int(input())
+    
     count = 0
+    
     for _ in range(n):
         cx, cy, cr = map(int, input().split())
-        dis1 = (x1 - cx)**2 + (y1 - cy)**2
-        dis2 = (x2 - cx)**2 + (y2 - cy)**2
-        pow_cr = cr**2
         
-        if pow_cr > dis1 and pow_cr > dis2:
-            pass
-        elif pow_cr > dis1:
+        d1 = (x1 - cx)**2 + (y1 - cy)**2
+        d2 = (x2 - cx)**2 + (y2 - cy)**2
+
+        if cr**2 > d1 and cr**2 < d2:
             count += 1
-        elif pow_cr > dis2:
+            
+        elif cr**2 > d2 and cr**2 < d1:
             count += 1
             
     print(count)
