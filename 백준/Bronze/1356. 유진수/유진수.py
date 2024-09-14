117fd2b5b62080bb2a8ced27c41d4299a1bf27e2
14857 <165680202+14857@users.noreply.github.com>
2024-09-15 02:13:58 +0900

    [Bronze I] Title: 유진수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1356. 유진수/유진수.py
+++ 백준/Bronze/1356. 유진수/유진수.py
@@ -1,17 +1,24 @@
-N = input()
-ok = 0
+# 유진수는 어떤 수를 10진수로 표현한 뒤 그 수를 두 부분으로 나눴을 때,
+# 앞부분 자리수의 곱과 뒷부분 자리수의 곱이 같을 때를 말한다.
 
-for i in range(1, len(N)):
-    s1, s2 = N[:i], N[i:]
-    m1 = m2 = 1
-    for n in s1:
-        m1 *= int(n)
+n = input()
+ans = 0
+
+for i in range(1,len(n)):
+    total1,total2 = 1,1
+    a,b = n[:i],n[i:]
     
-    for n in s2:
-        m2 *= int(n)
+    for i in a:
+        total1 *= int(i)
     
-    if m1 == m2:
-        ok = 1
-        break
-        
-print("YES" if ok else "NO")
+    for j in b:
+        total2 *= int(j)
+    
+    if(total1 == total2):
+        ans = 1
+
+if ans == 0:
+    print("NO")
+else:
+    print("YES")
+    
