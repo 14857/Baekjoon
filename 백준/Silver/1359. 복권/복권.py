fdb3df9f744b19b69e44cd8c5c39700c99ceb279
14857 <165680202+14857@users.noreply.github.com>
2024-09-19 23:58:59 +0900

    [Silver IV] Title: 복권, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1359. 복권/복권.py
+++ 백준/Silver/1359. 복권/복권.py
@@ -1,16 +1,24 @@
+# “1부터 N까지의 수 중에 서로 다른 M개의 수를 골라보세요.
+# 저희도 1부터 N까지의 수 중에 서로 다른 M개의 수를 고를건데, 적어도 K개의 수가 같으면 당첨입니다.
+# 지민이가 복권에 당첨될 확률을 구하는 프로그램
+# combination 원리 이용
+
 from itertools import combinations
 
 n, m, k = map(int,input().split())
 
 ans = 0
-all = [*combinations([i for i in range(n)], m)]  # 모든 경우의 수
+
+all = [*combinations(range(1,n+1), m)]  # 모든 경우의 수
 
 for i in all:
   cnt = 0
+  
   for j in range(m):
-    if i[j] < m:  # 0 ~ m-1이 복권에 당첨되는 번호라고 가정한다.
+    if i[j] <= m:  # 0 ~ m-1이 복권 당첨 번호라고 가정
       cnt+=1
+  
   if cnt >= k:  # k 개 이상 맞은 경우
     ans += 1
 
-print(ans / len(all))
+print(ans / len(all)) # 맞는 경우의 수/전체 경우의 수
