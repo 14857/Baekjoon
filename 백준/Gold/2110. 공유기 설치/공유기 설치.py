f8f636f4e135f476d530034045dc47292b4ede71
14857 <165680202+14857@users.noreply.github.com>
2024-11-12 00:12:32 +0900

    [Gold IV] Title: 공유기 설치, Time: 472 ms, Memory: 38848 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,34 +1,41 @@
-n, c = map(int, input().split())
+# 집의 개수 N (2 ≤ N ≤ 200,000)과 공유기의 개수 C (2 ≤ C ≤ N)
+# C개의 공유기를 N개의 집에 적당히 설치해서
+# 가장 인접한 두 공유기 사이의 거리를 최대로 하는 프로그램
+# 정렬, 이분탐색 (투 포인터 사용)
 
-array = []
-for i in range(n):
-    array.append(int(input()))
+import sys
+input = sys.stdin.readline
 
-array.sort()
+n,c = map(int,input().split())
 
+arr = []
 
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        current = array[0]
-        count = 1
+for _ in range(n):
+    arr.append(int(input()))
 
-        for i in range(1, len(array)):
-            if array[i] >= current + mid:
-                count += 1
-                current = array[i]
+arr.sort()
 
-        if count >= c:
-            global answer
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
-
-
+# 초기값 설정 -> 앞 집부터 공유기 설치
 start = 1
-end = array[-1] - array[0]
-answer = 0
+end = arr[-1] - arr[0]
+ans = 0
 
-binary_search(array, start, end)
-print(answer)
+while (start <= end):
+    mid = (start + end) // 2 # 설치 거리
+    current = arr[0]
+    count = 1
+    
+    for i in range(1,len(arr)):
+        if arr[i] >= current + mid:
+            count += 1
+            current = arr[i]
+    
+    if count >= c: # 더 넓게 설치할 수 있다는 의미 -> 설치 거리를 늘린
+        start = mid+1
+        ans = mid
+    
+    else: # 더 좁게 설치해야 한다는 의미 -> 설치 거리를 줄인다
+        end = mid-1
+    
+
+print(ans)
