b44d1aa800d7ce4b693cf75fdb05d86979ca4992
14857 <165680202+14857@users.noreply.github.com>
2025-11-07 00:09:37 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 16.45 ms, Memory: 64.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, n: Int): String {
+        var answer: String = ""
+        
+        answer = my_string.drop(my_string.length - n)
+        
+        return answer
+    }
+}
