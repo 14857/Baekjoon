29e9b2c469055cd0f8ee55963f2c13e262ab1773
14857 <165680202+14857@users.noreply.github.com>
2025-11-07 02:05:56 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(myString: String): String {
+        var answer: String = ""
+        
+        answer = myString.uppercase()
+        
+        return answer
+    }
+}
