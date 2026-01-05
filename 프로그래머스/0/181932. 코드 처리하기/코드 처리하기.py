0669507b28474a47e998dd2d5830e6d6cd2c3c4b
14857 <165680202+14857@users.noreply.github.com>
2026-01-05 16:09:05 +0900

    [level 0] Title: 코드 처리하기, Time: 19.15 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,16 @@
+def solution(code):
+    answer = ''
+    mode = 0
+    
+    for i in range (0,len(code)):
+        if code[i] == "1":
+            mode = 0 if mode == 1 else 1   
+        else:
+            if (i %2 == 0 and mode == 0):
+                answer +=(code[i])
+            elif (i %2 == 1 and mode == 1):
+                answer+=(code[i])
+    if len(answer) == 0:
+        answer = "EMPTY"
+    
+    return answer
