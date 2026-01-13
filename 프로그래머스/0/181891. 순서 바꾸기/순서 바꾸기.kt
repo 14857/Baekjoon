ca2daf886582519f77352e56e297217b8f00d272
14857 <165680202+14857@users.noreply.github.com>
2026-01-13 17:52:48 +0900

    [level 0] Title: 순서 바꾸기, Time: 43.39 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num_list: IntArray, n: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        
+        answer = num_list.slice(n until num_list.size).toIntArray() + num_list.slice(0..n-1).toIntArray()
+        
+        return answer
+    }
+}
