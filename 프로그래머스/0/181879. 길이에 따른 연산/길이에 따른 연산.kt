0b4160404630584835b49c7969cf1156593263de
14857 <165680202+14857@users.noreply.github.com>
2026-01-14 23:56:00 +0900

    [level 0] Title: 길이에 따른 연산, Time: 18.91 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(num_list: IntArray): Int {
+        var answer: Int = 0
+        
+        if(num_list.size >= 11){
+            answer = num_list.sum()
+        }
+        else{
+            answer = num_list.reduce { acc, v -> acc * v }
+        }
+        
+        return answer
+    }
+}
