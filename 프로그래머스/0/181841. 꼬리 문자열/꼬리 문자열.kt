0f73ece53428cba9e4c371c2790c3bb6d4e7c0a7
14857 <165680202+14857@users.noreply.github.com>
2026-01-20 23:36:02 +0900

    [level 0] Title: 꼬리 문자열, Time: 12.49 ms, Memory: 63.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(str_list: Array<String>, ex: String): String {
+        var answer: String = ""
+        
+        for(str in str_list){
+            if (ex !in str){
+                answer += str
+            }
+        }
+        
+        return answer
+    }
+}
