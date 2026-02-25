faebad412084c289738d8441ca9a9cdbd01ac189
14857 <165680202+14857@users.noreply.github.com>
2026-02-25 23:10:35 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 12.19 ms, Memory: 65.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, letter: String): String {
+        var answer: String = ""
+        
+        answer = my_string.replace(letter,"")
+        
+        return answer
+    }
+}
