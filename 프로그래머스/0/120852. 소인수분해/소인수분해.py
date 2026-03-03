2c3e525ee068afef6b36d8df779b87791470934b
14857 <165680202+14857@users.noreply.github.com>
2026-03-03 22:26:21 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,15 @@
+def solution(n):
+    answer = []
+    i = 2
+    
+    while i * i <= n:
+        if n % i == 0:
+            answer.append(i)
+            n //= i
+        else:
+            i += 1
+    
+    if n > 1:
+        answer.append(n)
+        
+    return sorted(list(set(answer)))
