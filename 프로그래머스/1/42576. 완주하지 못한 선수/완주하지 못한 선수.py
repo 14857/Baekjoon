6e937ba74f5aa4450fd9e637de5118d2a4fe1920
14857 <165680202+14857@users.noreply.github.com>
2026-04-24 23:39:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.04 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+
+    dic = {}
+    
+    for i in participant:
+        if i not in dic:
+            dic[i] = 1
+        else:
+            dic[i] += 1
+    
+    for i in completion:
+        dic[i] -= 1
+        
+        
+    for i in dic:
+        if(dic[i] > 0):
+            return i
+            
